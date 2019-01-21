50af7838c7cb470ba3ef2153adbeead91ed4f651
qavk <aan5@mail.ru>
2019-01-22 00:53:15 +0300

    Stable build with encryption

---
+++ encryption.py
@@ -0,0 +1,81 @@
+class SpeckCipher(object):
+    """Speck Block Cipher Object"""
+
+    def encrypt_round(self, x, y, k):
+        """Complete One Round of Feistel Operation"""
+        rs_x = ((x << (self.word_size - self.alpha_shift)) + (x >> self.alpha_shift)) & self.mod_mask
+        add_sxy = (rs_x + y) & self.mod_mask
+        new_x = k ^ add_sxy
+        ls_y = ((y >> (self.word_size - self.beta_shift)) + (y << self.beta_shift)) & self.mod_mask
+        new_y = new_x ^ ls_y
+        return new_x, new_y
+
+    def __init__(self, key):
+
+        self.key_size = 128
+        self.block_size = 128
+        self.word_size = self.block_size >> 1
+        self.rounds = 32
+        self.mod_mask = (2 ** self.word_size) - 1
+        self.mod_mask_sub = (2 ** self.word_size)
+        self.beta_shift = 3
+        self.alpha_shift = 8
+
+        self.key = key & ((2 ** self.key_size) - 1)
+
+        # Pre-compile key schedule
+        self.key_schedule = [self.key & self.mod_mask]
+        l_schedule = [(self.key >> (x * self.word_size)) & self.mod_mask for x in
+                      range(1, self.key_size // self.word_size)]
+
+        for x in range(self.rounds - 1):
+            new_l_k = self.encrypt_round(l_schedule[x], self.key_schedule[x], x)
+            l_schedule.append(new_l_k[0])
+            self.key_schedule.append(new_l_k[1])
+
+    def encrypt(self, plaintext):
+        b = (plaintext >> self.word_size) & self.mod_mask
+        a = plaintext & self.mod_mask
+
+        b, a = self.encrypt_function(b, a)
+
+        ciphertext = (b << self.word_size) + a
+        return ciphertext
+
+    def decrypt(self, ciphertext):
+        b = (ciphertext >> self.word_size) & self.mod_mask
+        a = ciphertext & self.mod_mask
+
+        b, a = self.decrypt_function(b, a)
+
+        plaintext = (b << self.word_size) + a
+        return plaintext
+
+    def encrypt_function(self, upper_word, lower_word):
+        x = upper_word
+        y = lower_word
+
+        # Run Encryption Steps For Appropriate Number of Rounds
+        for k in self.key_schedule:
+            rs_x = ((x << (self.word_size - self.alpha_shift)) + (x >> self.alpha_shift)) & self.mod_mask
+            add_sxy = (rs_x + y) & self.mod_mask
+            x = k ^ add_sxy
+            ls_y = ((y >> (self.word_size - self.beta_shift)) + (y << self.beta_shift)) & self.mod_mask
+            y = x ^ ls_y
+        return x, y
+
+    def decrypt_function(self, upper_word, lower_word):
+        x = upper_word
+        y = lower_word
+
+        # Run Encryption Steps For Appropriate Number of Rounds
+        for k in reversed(self.key_schedule):
+            xor_xy = x ^ y
+            y = ((xor_xy << (self.word_size - self.beta_shift)) + (xor_xy >> self.beta_shift)) & self.mod_mask
+            xor_xk = x ^ k
+            msub = ((xor_xk - y) + self.mod_mask_sub) % self.mod_mask_sub
+            x = ((msub >> (self.word_size - self.alpha_shift)) + (msub << self.alpha_shift)) & self.mod_mask
+        return x, y
+
+
+
--- stegano.py
+++ stegano.py
@@ -1,4 +1,5 @@
 import argparse
+from encryption import SpeckCipher
 
 
 def create_parser():
@@ -6,15 +7,15 @@ def create_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--container', required=True)
     parser.add_argument('-m', '--message')
-    parser.add_argument('-k', '--key', default=12345)
+    parser.add_argument('-k', '--key', default="fjJD2Yf3FNJ1933wy532fdsd30283HhfbFHEjsfgycurcn372t7f284dg2d27egf624")
     return parser
 
 
-def check_input(container, message, key):
+def check_input(container, message):
     """Проверка введённых пользователем данных"""
-    if container and message and container.endswith(".txt") and message.endswith(".txt") and str(key).isdigit():
+    if container and message and container.endswith(".txt") and message.endswith(".txt"):
         return 1
-    elif container and not message and container.endswith(".txt") and str(key).isdigit():
+    elif container and not message and container.endswith(".txt"):
         return 2
     else:
         exit("Введённые данные некорректны!")
@@ -24,6 +25,21 @@ def check_container(container, message):
     """Проверка вместимости контейнера"""
     if len(message) > container.count(" "):
         exit("Сообщение не поместится в данный контейнер!")
+
+
+def convert_key(key):
+    """Преобразование исходного ключа в криптографический и стеганографический ключи"""
+    if len(key) < 10:
+        key = "fjJD2Yf3FNJ1933wy532fdsd30283HhfbFHEjsfgycurcn372t7f284dg2d27egf624"
+        print(f"Длина введённого ключа менее 10 символов.\n"
+              f"Установлен ключ по умолчанию: {key}")
+
+    crypto_key = key[0:len(key)//2]
+    stego_key = key[len(key)//2:]
+
+    crypto_key_int = sum([ord(c) << (8 * x) for x, c in enumerate(reversed(crypto_key))])
+    stego_key_int = sum([ord(c) << (8 * x) for x, c in enumerate(reversed(stego_key))])
+    return crypto_key_int, stego_key_int
 
 
 def lin_rand_arr():
@@ -98,7 +114,7 @@ def destego_message(container, key):
         count = g_random_seed
         while count > 0:
             if len(last_byte) == 8:
-                if num_of_spaces >= 8:
+                if num_of_spaces >= 8 and (last_byte == "11111111" or last_byte == "00000000"):
                     exit_flag = True
                     break
                 message += last_byte
@@ -138,10 +154,92 @@ def bin2text(message):
     return res
 
 
+def prepare_blocks(message):
+    """Разбиение секретного сообщения на блоки"""
+    blocks = []
+    step = 16
+
+    if len(message) % 16 == 0:
+        blocks_count = len(message) // 16
+        for i in range(blocks_count):
+            block = message[i*16:step]
+            step += 16
+            blocks.append(block)
+        last_block = chr(128) + "0" * 15
+        blocks.append(last_block)
+    else:
+        blocks_count = len(message) // 16 + 1
+        for i in range(blocks_count-1):
+            block = message[i*16:step]
+            step += 16
+            blocks.append(block)
+
+        last_block_count = 16 - (blocks_count * 16 - len(message))
+        last_block = message[(blocks_count-1) * 16: (blocks_count-1) * 16 + last_block_count] + \
+            chr(128) + "0" * (blocks_count * 16 - len(message) - 1)
+        blocks.append(last_block)
+
+    for i in range(len(blocks)):
+        blocks[i] = sum([ord(c) << (8 * x) for x, c in enumerate(reversed(blocks[i]))])
+    return blocks
+
+
+def encrypt_message(message, key):
+    """Шифрование секретного сообщения"""
+    encrypted_message = ""
+    cipher = SpeckCipher(key)
+
+    blocks_count = len(message) // 16 + 1
+    blocks = prepare_blocks(message)
+
+    for i in range(blocks_count):
+        encrypted_block = cipher.encrypt(blocks[i])
+        encrypted_message_bytes = bytearray.fromhex('{:08x}'.format(encrypted_block))
+        for byte in encrypted_message_bytes:
+            encrypted_message += chr(byte)
+    return encrypted_message
+
+
+def decrypt_message(encrypted_message, key):
+    """Расшифровывание секретного сообщения"""
+    decrypted_message = ""
+    step = 16
+    cipher = SpeckCipher(key)
+
+    # print(len(encrypted_message))
+    blocks_count = len(encrypted_message) // 16
+
+    for i in range(blocks_count):
+        block = encrypted_message[i * 16:step]
+
+        encrypted_block_int = sum([ord(c) << (8 * x) for x, c in enumerate(reversed(block))])
+        decrypted_block_int = cipher.decrypt(encrypted_block_int)
+        decrypted_block_bytes = bytearray.fromhex('{:032x}'.format(decrypted_block_int))
+
+        if i == blocks_count - 1:
+            for j in range(len(decrypted_block_bytes)):
+                if decrypted_block_bytes[j] == 0x80:
+                    k = j
+            for j in range(0, k):
+                decrypted_message += chr(decrypted_block_bytes[j])
+        else:
+            for j in range(len(decrypted_block_bytes)):
+                decrypted_message += chr(decrypted_block_bytes[j])
+        step += 16
+
+    return decrypted_message
+
+
 def get_message(path):
     """Помещение содержимого файла с сообщением в переменную"""
+    message = ""
     with open(path, "r") as file:
-        message = file.read()
+        for line in file:
+            for char in line:
+                if ord(char) == 10:
+                    message += chr(13) + chr(10)
+                else:
+                    message += char
     if not message:
         exit("Скрываемое сообщение не может быть пустым!")
     return message
@@ -157,7 +255,9 @@ def get_container(path):
 def put_message(message):
     """Запись извлечённого сообщения в файл"""
     with open("secret_message.txt", "w") as file:
-        file.write(message)
+        for char in message:
+            if ord(char) != 13:
+                file.write(char)
 
 
 def _main():
@@ -168,10 +268,13 @@ def _main():
 
     path_to_container = namespace.container
     path_to_message = namespace.message
-    key = int(namespace.key)
+    key = namespace.key
+
+    # преобразуем пользовательский ключ в криптографический и стеганографический ключи
+    crypto_key, stego_key = convert_key(key)
 
     # проверка правильности введённых аргументов и определение типа операции
-    operation_type = check_input(path_to_container, path_to_message, key)
+    operation_type = check_input(path_to_container, path_to_message)
 
     # извлечение контейнера из файла
     container = get_container(path_to_container)
@@ -179,6 +282,11 @@ def _main():
     if operation_type == 1:
         # извлечение сообщения из файла
         message = get_message(path_to_message)
+        # print(f"Секретное сообщение:\n{message}")
+
+        # шифрование сообщения
+        message = encrypt_message(message, crypto_key)
+        # print(f"Зашифрованное сообщение: {message}")
 
         # преобразование сообщения в двочиный код
         message = text2bin(message)
@@ -187,18 +295,22 @@ def _main():
         check_container(container, message)
 
         # встраивание сообщения в контейнер
-        stego_message(container, message, key)
+        stego_message(container, message, stego_key)
 
     elif operation_type == 2:
         # извлечение сообщения из контейнера
-        message = destego_message(container, key)
+        message = destego_message(container, stego_key)
 
         # преобразование двоичного кода в десятичный
         message = bin2text(message)
+        # print(f"Зашифрованное сообщение: {message},\nДлина: {len(message)}")
+
+        # расшифрование секретного сообщения
+        message = decrypt_message(message, crypto_key)
 
         # запись извлечённого сообщения в файл и вывод его на экран
         put_message(message)
-        print(message)
+        print(f"Секретное сообщение:\n{message}")
 
 
 if __name__ == "__main__":
